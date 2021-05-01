45143536e4fb1067768bd4b671632f9bb1750eea
Matt Macdonald Rasp4 <mksmacdonald@gmail.com>
2021-05-01 09:17:21 -0700

    update query get all properties

--- server/database.js
+++ server/database.js
@@ -55,7 +55,7 @@ const getUserWithId = function (id) {
       [id]
     )
     .then((result) => {
-       console.log(result.rows[0]);
+      console.log(result.rows[0]);
       if (result) {
         return result.rows[0];
       }
@@ -73,26 +73,25 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  
   console.log(user);
   return pool
-  .query(
-    `
+    .query(
+      `
 INSERT INTO users VALUES
 (DEFAULT, $1, $2, $3)
 `,
-    [user.name, user.email, user.password]
-  )
-  .then((result) => {
-    // console.log(result.rows);
-    if (result) {
-      return result.rows;
-    }
-    return null;
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+      [user.name, user.email, user.password]
+    )
+    .then((result) => {
+      // console.log(result.rows);
+      if (result) {
+        return result.rows;
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
   // const userId = Object.keys(users).length + 1;
   // user.id = userId;
   // users[userId] = user;
@@ -108,30 +107,30 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  console.log('this ran');
-  
+  console.log("this ran");
+
   return pool
-  .query(
-    `
+    .query(
+      `
 SELECT * 
 FROM reservations
 WHERE guest_id = $1
 LIMIT $2
 `,
-  [guest_id, limit]
-  )
-  .then((result) => {
-    if (result) {
-      // console.log(`getAllReservations for ${guest_id} with a limit of ${limit}`);
-      // console.log( result.rows[0] );
-      return result.rows[0];
-    }
-    console.log("getAllReservations returned no results");
-    return null;
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+      [guest_id, limit]
+    )
+    .then((result) => {
+      if (result) {
+        // console.log(`getAllReservations for ${guest_id} with a limit of ${limit}`);
+        // console.log( result.rows[0] );
+        return result.rows[0];
+      }
+      console.log("getAllReservations returned no results");
+      return null;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
@@ -144,23 +143,53 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      return result.rows[0];
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+const getAllProperties = function (options, limit = 10) {
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {   
+    // console.log("BEFORE options.city : ",options.city);
+    options.city = options.city.slice(1 , options.city.length - 1 )
+    // console.log("AFTER options.city : ",options.city);
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+
+
+  // owner ID
+  if (options.owner_id) {
+    if(queryString.contains('WHERE')) {
+      queryString += 'AND ';
+    } else {
+      queryString += 'WHERE ';
+    }
+
+    queryParams.push(`%${options.owner_id}%`);
+    queryString += `owner_id LIKE $${queryParams.length} `;
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
-// const getAllProperties = function(options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// }
 exports.getAllProperties = getAllProperties;
 
 /**
